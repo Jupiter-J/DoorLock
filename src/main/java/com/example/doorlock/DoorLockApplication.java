2f91a8c4aca590b1ec2515ea1e3f6547bb239d3f
Jupiter-J <namju2912@gmail.com>
2021-12-01 23:04:29 +0900

    init

---
+++ src/main/java/com/example/doorlock/DoorLockApplication.java
@@ -0,0 +1,13 @@
+package com.example.doorlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoorLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoorLockApplication.class, args);
+    }
+
+}
